1f7b265cfff5f0a7e6e89662bc6da2ecbb081e6e
Rafaela2005 <131188177+Rafaela2005@users.noreply.github.com>
2023-04-18 16:22:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcular () {
+var numero1 = document.getElementsByClassName('input1').value;
+var numero2 = document.getElementsByClassName('input2').value;
+
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1 / numero2;
+
+document.getElementsByClassName('soma').innerHTML = soma;
+
+}
